4d0cdfd779b7a473423d9c60df08be5d168b2f4e
katerina <you-like-me@yandex.ru>
2022-10-03 22:26:41 +0300

    Add home work 2 task003

--- Home_work_3/task003.py
+++ Home_work_3/task003.py
@@ -8,6 +8,6 @@
 list1 = [1.1, 1.2, 3.1, 5, 10.01]
 list2 = []
 for i in range(len(list1)):
-    if round(list1[i] - int(list1[i])) != 0:
+    if list1[i] - int(list1[i]) != 0:
         list2.append(round((list1[i] - int(list1[i])), 2))
 print(max(list2) - min(list2))
