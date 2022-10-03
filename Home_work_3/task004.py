008f7dd125a682de22b311c7d694f1e361d69652
katerina <you-like-me@yandex.ru>
2022-10-03 22:35:10 +0300

    Add home work 2 task004

--- Home_work_3/task004.py
+++ Home_work_3/task004.py
@@ -6,17 +6,7 @@
 # - 3 -> 11
 # - 2 -> 10
 
-# n=int(input())
-# m=n
-# p=1
-# d1=0
-# while m>0:
-#     d1=d1+m%2*p
-#     p=p*10
-#     m=m//2
-# print(d1)
-
-x = 150
+x = 45
 y = str(bin(x))
-print(y[2:])
+print(x, y[2:], sep= '->')
 
